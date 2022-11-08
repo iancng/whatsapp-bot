d64ae730be0fd13fa40668ad43e0ec8e4dad2850
iancng <iancng2@gmail.com>
2022-11-08 20:41:28 +0800

    add sandbox

--- server.cjs
+++ server.cjs
@@ -1,5 +1,10 @@
 const { Client } = require('whatsapp-web.js');
-const client = new Client();
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+		args: ['--no-sandbox'],
+	}
+});
 
 client.on('qr', (qr) => {
     console.log('QR RECEIVED', qr);
