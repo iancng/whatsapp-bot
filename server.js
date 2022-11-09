a70d7fd63e305ce5a145181f797849774337f413
iancng <iancng2@gmail.com>
2022-11-09 17:34:59 +0800

    add

--- server.js
+++ server.js
@@ -6,22 +6,7 @@ import moment from 'moment';
 import { XMLParser } from "fast-xml-parser";
 
 const { Client, LocalAuth } = whatsapp_web;
-
-async function checkTrafficNews(msg) {
-    const response = await fetch('https://www.td.gov.hk/tc/special_news/trafficnews.xml');
-    const parser = new XMLParser();
-    const XMLdata = await response.text();
-    const XMLobject = parser.parse(XMLdata);
-    const data = XMLobject.list.message;
-    console.log(data);
-    const anDate = new Date(data.ANNOUNCEMENT_DATE);
-    const dateText = moment(anDate).format("HH:mm")
-    const message = `*${data.INCIDENT_STATUS_CN}* ${dateText}
-*${data.INCIDENT_HEADING_CN}*(${data.INCIDENT_DETAIL_CN})
-${data.DIRECTION_CN} ${data.LOCATION_CN}
-${data.CONTENT_CN}`
-    msg.reply(message);
-}
+let myID = '85295860339@c.us';
 
 const client = new Client({
     puppeteer: {
@@ -42,13 +27,107 @@ client.on('ready', () => {
     console.log('Client is ready!');
 });
 
-client.on('message', msg => {
-    if (msg.body == '!ping') {
+client.on('message', async msg => {
+    if (msg.body == '!help'){
+        msg.reply('Enter the following command:\n!tn - Traffic News\n!ping - Pong\n');
+    }
+    else if (msg.body == '!ping') {
         msg.reply('pong');
     }
-    if (msg.body == '!tn') {
-        checkTrafficNews(msg);
+    else if (msg.body == '!tn') {
+        checkTrafficNews(msg, client);
+    }
+    else if (msg.body == '!info'){
+        let chat = await msg.getChat();
+        console.log(chat.id);
+        msg.reply(`Chat ID: ${chat.id}\nChat Name:${chat.name}`)
+    }
+    else if (msg.body == '!kickgroup'){
+        let chat = await msg.getChat();
+        let groupChat = await client.getChatById(chat.id._serialized);
+        let clientId = client.info.wid._serialized;
+        if(chat.isGroup){
+            let participants = groupChat.participants;
+            let listOfRemove = [];
+            participants.forEach(participant => {
+                let partId = participant.id._serialized;
+                if(partId != clientId && partId != myID){
+                    listOfRemove.push(partId);
+                }
+            });
+            groupChat.removeParticipants(listOfRemove);
+            if(listOfRemove.length!=0){
+                msg.reply("Removed " + listOfRemove)
+            }
+        }
+        else{
+            msg.reply('You must be in group to do so.')
+        }
+    }
+    else if (msg.body == '!promoteall'){
+        let chat = await msg.getChat();
+        let groupChat = await client.getChatById(chat.id._serialized);
+        let clientId = client.info.wid._serialized;
+        if(chat.isGroup){
+            let participants = groupChat.participants;
+            let partIdList = [];
+            participants.forEach(participant=>{
+                let partId = participant.id._serialized;
+                partIdList.push(partId);
+            })
+            groupChat.promoteParticipants(partIdList);
+            if(partIdList.length!=0){
+                msg.reply("All promoted to admin");
+            }
+        }
+        else{
+            msg.reply("You must be in group to do so.")
+        }
+    }
+    else if (msg.body == '!demoteall'){
+        let chat = await msg.getChat();
+        let groupChat = await client.getChatById(chat.id._serialized);
+        let clientId = client.info.wid._serialized;
+        if(chat.isGroup){
+            let participants = groupChat.participants;
+            let partIdList = [];
+            participants.forEach(participant=>{
+                let partId = participant.id._serialized;
+                partIdList.push(partId);
+            })
+            groupChat.demoteParticipants(partIdList);
+            if(partIdList.length!=0){
+                msg.reply("All demoted back to user");
+            }
+        }
+        else{
+            msg.reply("You must be in group to do so.")
+        }
+    }
+    else if(msg.body == '!diu'){
+        msg.reply('diu nei');
     }
 });
 
 client.initialize();
+
+
+//All functions are below
+
+async function checkTrafficNews(msg, client) {
+    const info = client.info;
+    const messageSentTime = moment().format("YYYY-MM-DD HH:mm");
+    console.log(`${messageSentTime} ${msg.from} sent !tn`);
+    const response = await fetch('https://www.td.gov.hk/tc/special_news/trafficnews.xml');
+    const parser = new XMLParser();
+    const XMLdata = await response.text();
+    const XMLobject = parser.parse(XMLdata);
+    const data = XMLobject.list.message;
+    const anDate = new Date(data.ANNOUNCEMENT_DATE);
+    const dateText = moment(anDate).format("HH:mm")
+    const message = `*${data.INCIDENT_STATUS_CN}* ${dateText}
+*${data.INCIDENT_HEADING_CN}*(${data.INCIDENT_DETAIL_CN})
+${data.DIRECTION_CN} ${data.LOCATION_CN}
+${data.CONTENT_CN}`
+    msg.reply(message);
+}
